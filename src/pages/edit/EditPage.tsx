1da5d3900340a40ce0053a5b4515472fb1380e70
ccyanxyz <ccyanxyz@gmail.com>
2021-12-16 21:55:29 +0800

    update

--- src/pages/edit/EditPage.tsx
+++ src/pages/edit/EditPage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Editor from "rich-markdown-editor";
+
 export const EditPage: React.FC = () => {
   
   return(
     <>
       <div>
-        
+        <Editor
+          defaultValue="Hello world!"
+        />
       </div>
     </>
   )
