972f9d9b8b2278f1f5530cda338adb31271bf974
codeaunt <zhouzili98@hotmail.com>
2022-05-16 20:29:39 +0800

    add follower layout

--- backend/did/dfusion/dfusion.did.d.ts
+++ backend/did/dfusion/dfusion.did.d.ts
@@ -10,19 +10,21 @@ export interface DFusion {
       arg_0: Principal,
       arg_1: Array<Principal>,
     ) => Promise<undefined>,
-  'createEntry' : (arg_0: CreateEntryRequest) => Promise<Result_3>,
-  'favor' : (arg_0: bigint) => Promise<Result_1>,
+  'createEntry' : (arg_0: CreateEntryRequest) => Promise<Result_1>,
+  'favor' : (arg_0: bigint) => Promise<Result_2>,
   'follow' : (arg_0: Principal) => Promise<boolean>,
   'getBucketInfo' : () => Promise<
       [Array<bigint>, Array<bigint>, Array<Principal>]
     >,
   'getBucketStableSize' : (arg_0: Principal) => Promise<bigint>,
   'getEntries' : (arg_0: number, arg_1: number) => Promise<Array<EntryDigest>>,
-  'getEntry' : (arg_0: bigint) => Promise<Result_2>,
+  'getEntry' : (arg_0: bigint) => Promise<Result_3>,
   'getUser' : (arg_0: Principal) => Promise<[] | [UserExt]>,
   'getUserEntries' : (arg_0: Principal) => Promise<Array<EntryDigest>>,
   'getUserFavorites' : (arg_0: Principal) => Promise<Array<EntryDigest>>,
-  'like' : (arg_0: bigint) => Promise<Result_1>,
+  'getUserFollowingEntries' : (arg_0: Principal) => Promise<Array<EntryDigest>>,
+  'like' : (arg_0: bigint) => Promise<Result_2>,
+  'mintNFT' : (arg_0: bigint) => Promise<Result_1>,
   'removeAuth' : (arg_0: Principal) => Promise<boolean>,
   'setLimit' : (arg_0: SetConfigRequest) => Promise<boolean>,
   'setUserInfo' : (arg_0: SetUserRequest) => Promise<Result>,
@@ -51,11 +53,11 @@ export interface EntryExt {
 }
 export type Result = { 'ok' : null } |
   { 'err' : string };
-export type Result_1 = { 'ok' : boolean } |
+export type Result_1 = { 'ok' : bigint } |
   { 'err' : string };
-export type Result_2 = { 'ok' : EntryExt } |
+export type Result_2 = { 'ok' : boolean } |
   { 'err' : string };
-export type Result_3 = { 'ok' : bigint } |
+export type Result_3 = { 'ok' : EntryExt } |
   { 'err' : string };
 export interface SetConfigRequest {
   'coverLimit' : [] | [bigint],
