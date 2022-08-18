4cea819d9aaa8576aeec150859acacd22de27dec
codeaunt <zhouzili98@hotmail.com>
2022-08-18 09:26:51 +0800

    add meta for twitter:card

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,13 +2,17 @@ export const ENV = {
   host: process.env.HOST || "https://ic0.app",
   canisterIds: {
     dfusion: process.env.DFUSION_CANISTER_ID || 
-    'br67l-yiaaa-aaaai-qkjra-cai',
+    // 'br67l-yiaaa-aaaai-qkjra-cai', test
+    'itxs3-oiaaa-aaaai-qniua-cai',
     index: process.env.INDEX_CANISTER_ID || 
-    "dsgd3-3yaaa-aaaai-qmuua-cai",
+    // "dsgd3-3yaaa-aaaai-qmuua-cai", test 
+    "i5v7t-vyaaa-aaaai-qniva-cai",
     nft: process.env.NFT_CANISTER_ID ||
-    "wrciq-pqaaa-aaaai-qleya-cai", 
+    // "wrciq-pqaaa-aaaai-qleya-cai", test
+    "i2uzh-yaaaa-aaaai-qnivq-cai",
     notify: process.env.NOTIFY_CANISTER_ID ||
-    "c2k5h-zqaaa-aaaai-qlm6q-cai",
+    // "c2k5h-zqaaa-aaaai-qlm6q-cai", test 
+    "iuwup-dqaaa-aaaai-qniuq-cai",
     reverse_registrar: process.env.REVERSE_TRGISTRAR_CANISTER_ID || 
     'etiyd-ciaaa-aaaan-qabbq-cai',
   },
